b72514abdd70a2e281900ee2ddae5dec19ee0d47
mnichangxin <mnichangxin@163.com>
2018-03-01 22:48:04 +0800

    commit

--- app/src/main/java/com/lichangxin/xiuchat/ViewSlideActivity.java
+++ app/src/main/java/com/lichangxin/xiuchat/ViewSlideActivity.java
@@ -1,17 +1,67 @@
 package com.lichangxin.xiuchat;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewSlideActivity extends AppCompatActivity {
+    private View view1;
+    private View view2;
+    private View view3;
+    private ViewPager viewPager;
+    private ArrayList<View> viewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_slide);
+        setContentView(R.layout.view_page);
 
         LayoutInflater inflater = getLayoutInflater().from(this);
-        
+
+        view1 = inflater.inflate(R.layout.view_page1, null);
+        view2 = inflater.inflate(R.layout.view_page2, null);
+        view3 = inflater.inflate(R.layout.view_page3, null);
+
+        viewList = new ArrayList<View>();
+        viewList.add(view1);
+        viewList.add(view2);
+        viewList.add(view3);
+
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new ViewPagerAdapter(viewList));
+        viewPager.setCurrentItem(0);
+    }
+
+    public class ViewPagerAdapter extends PagerAdapter {
+        private List<View> mListViews;
+
+        public ViewPagerAdapter(List<View> mListViews) {
+            this.mListViews = mListViews;
+        }
+
+        @Override
+        public int getCount() {
+            return mListViews.size();
+        }
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mListViews.get(position), 0);
+            return mListViews.get(position);
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mListViews.get(position));
+        }
     }
 }
